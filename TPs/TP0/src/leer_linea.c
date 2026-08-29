8430228f0e4cacfa472944e6432045f2279697b2
arturoguerrab <arturoguerrab12@gmail.com>
2026-08-29 19:44:05 -0300

    Coreccion de errores del tp0. se corrige que reciba el '\n', y validar en la funcion que los parametros no sean nulos. ademas si el tamaño recibido es 0 se coloca el bloque por defecto. Solventado el format con clang-format. Entrega paso todas las pruebas

--- TPs/TP0/src/leer_linea.c
+++ TPs/TP0/src/leer_linea.c
@@ -2,48 +2,65 @@
 #include <stdio.h>
 #define BLOQUE_MEMORIA 6
 
-char *leer_linea() {
-  size_t tamanio = BLOQUE_MEMORIA;
-  int ml;
-  char *ptr = malloc(sizeof(char) * tamanio);
-
-  if (ptr != NULL) {
-    ml = leer_linea_ptr(&ptr, &tamanio);
-    if (ml == -1) {
-      free(ptr);
-      ptr = NULL;
-    }
-  } else
-    ptr = NULL;
-
-  return ptr;
+char *leer_linea()
+{
+	size_t tamanio = BLOQUE_MEMORIA;
+	int ml;
+	char *ptr = malloc(sizeof(char) * tamanio);
+	if (ptr != NULL) {
+		ml = leer_linea_ptr(&ptr, &tamanio);
+		if (ml == -1) {
+			free(ptr);
+			ptr = NULL;
+		}
+	} else {
+		ptr = NULL;
+	}
+	return ptr;
 }
 
-int leer_linea_ptr(char **ptr, size_t *tamanio) {
-  int ml = 0;
-  int entrada = getchar();
-  char *aux = NULL;
+int leer_linea_ptr(char **ptr, size_t *tamanio)
+{
+	int ml = 0;
+	int entrada = 0;
+	char *aux = NULL;
+	if (ptr == NULL || tamanio == NULL) {
+		ml = -1;
+	} else {
+		entrada = getchar();
+		if (entrada == EOF) {
+			ml = -1;
+		}
+	}
 
-  while (entrada != '\n' && entrada != EOF && ml != -1) {
-    if (ml >= (int)*tamanio - 1) {
-      aux = realloc(*ptr, sizeof(char) * ((*tamanio) * 2));
-      if (aux != NULL) {
-        *ptr = aux;
-        (*tamanio) = (*tamanio) * 2;
-      } else
-        ml = -1;
-    }
+	while (entrada != EOF && ml != -1) {
+		if (ml >= (int)*tamanio - 1) {
+			if (*tamanio == 0) {
+				*tamanio = BLOQUE_MEMORIA;
+			}
+			aux = realloc(*ptr, sizeof(char) * ((*tamanio) * 2));
+			if (aux != NULL) {
+				*ptr = aux;
+				(*tamanio) = (*tamanio) * 2;
+			} else {
+				ml = -1;
+			}
+		}
 
-    if (ml != -1) {
-      (*ptr)[ml] = (char)entrada;
+		if (ml != -1) {
+			(*ptr)[ml] = (char)entrada;
+			ml++;
+			if (entrada == '\n') {
+				entrada = EOF;
+			} else {
+				entrada = getchar();
+			}
+		}
+	}
 
-      ml++;
-      entrada = getchar();
-    }
-  }
+	if (ml != -1) {
+		(*ptr)[ml] = '\0';
+	}
 
-  if (ml != -1)
-    (*ptr)[ml] = '\0';
-
-  return ml;
+	return ml;
 }
--- TPs/TP0/src/main.c
+++ TPs/TP0/src/main.c
@@ -1,10 +1,11 @@
 #include "leer_linea.h"
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
-  printf("Ingresa tu nombre: ");
-  char *nombre = leer_linea();
-  printf("Hola %s", nombre);
-  free(nombre);
-  return 0;
+int main(int argc, char *argv[])
+{
+	printf("Ingresa tu nombre: ");
+	char *nombre = leer_linea();
+	printf("Hola %s", nombre);
+	free(nombre);
+	return 0;
 }
